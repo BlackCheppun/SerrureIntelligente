5ae09c60b4484872fa6b50fdef40ac740354010c
BlackCheppun <aghouilesilyes1@gmail.com>
2025-02-12 22:49:01 +0100

    added register user api call

--- src/services/lock.service.ts
+++ src/services/lock.service.ts
@@ -6,5 +6,17 @@ const mmkv = new MMKVLoader().initialize();
 
 export class lockService {
 
+    static async registerUser() {
+        try {
+
+            const response = await axiosClient.post("/RegisterUser");
+            if (response.status == 200) {
+                return response.data.userid;
+            }
+            console.log(response);
+        } catch (error) {
+            console.log(error); // Handle error
+        }
+    }
 
 }
