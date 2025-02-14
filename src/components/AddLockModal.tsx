cf8b50dddff5a64a5b0f49f820e586b85c11b70d
BlackCheppun <aghouilesilyes1@gmail.com>
2025-02-14 10:16:31 +0100

    added set visible to false when clicked outside of modal

--- src/components/AddLockModal.tsx
+++ src/components/AddLockModal.tsx
@@ -1,10 +1,12 @@
-import { Modal, Pressable, StyleSheet, TextInput, useColorScheme, View } from "react-native";
+import { Modal, Pressable, StyleSheet, TextInput, TouchableWithoutFeedback, useColorScheme, View } from "react-native";
 import { darkTheme, lightTheme } from "../types/themes";
 import { Text } from "@react-navigation/elements";
 
 
+
 interface AddLockModalProps {
     visible: boolean;
+    setVisible: (val: boolean) => void;
     onClose: () => void;
     identifier: string;
     setIdentifier: (text: string) => void;
@@ -15,44 +17,45 @@ interface AddLockModalProps {
 }
 
 
-export default function AddLockModal({ visible, onClose, identifier, setIdentifier, password, setPassword, label, setLabel }: AddLockModalProps) {
+export default function AddLockModal({ visible, setVisible, onClose, identifier, setIdentifier, password, setPassword, label, setLabel }: AddLockModalProps) {
 
     const theme = useColorScheme() == 'dark' ? darkTheme : lightTheme;
 
 
-
-
     return (
+
         <Modal
             animationType="fade"
             transparent={true}
             visible={visible}
             onRequestClose={onClose}
         >
-            <View style={[styles.centeredView]}>
+            <TouchableWithoutFeedback onPress={() => setVisible(false)}>
+                <View style={[styles.centeredView]}>
 
-                <View style={[styles.container, { backgroundColor: theme.background }]}>
-                    <View style={styles.titleContainer}>
-                        <Text style={[{ color: theme.fontcolor }, styles.title]}>Ajouter une serrure</Text>
-                    </View>
-                    <View style={styles.innerInputContainer}>
-                        <Text style={[styles.subtitle, { color: theme.fontcolor }]}>Identifiant de la serrure</Text>
-                        <TextInput style={[styles.inputContainer, { borderColor: theme.fontcolor }]} placeholder="Identifiant de la serrure" value={identifier} onChangeText={setIdentifier} />
-                    </View>
-                    <View style={styles.innerInputContainer}>
-                        <Text style={[styles.subtitle, { color: theme.fontcolor }]}>Mot de passe de la serrure</Text>
-                        <TextInput style={[styles.inputContainer, { borderColor: theme.fontcolor }]} secureTextEntry={true} placeholder="mot de passe de la serrure" defaultValue="password" value={password} onChangeText={setPassword} />
-                    </View>
-                    <View style={styles.innerInputContainer}>
-                        <Text style={[styles.subtitle, { color: theme.fontcolor }]}>Label personnalisé</Text>
-                        <TextInput style={[styles.inputContainer, { borderColor: theme.fontcolor }]} placeholder="Label de votre serrure" value={label} onChangeText={setLabel} />
-                    </View>
+                    <View style={[styles.container, { backgroundColor: theme.background }]}>
+                        <View style={styles.titleContainer}>
+                            <Text style={[{ color: theme.fontcolor }, styles.title]}>Ajouter une serrure</Text>
+                        </View>
+                        <View style={styles.innerInputContainer}>
+                            <Text style={[styles.subtitle, { color: theme.fontcolor }]}>Identifiant de la serrure</Text>
+                            <TextInput style={[styles.inputContainer, { borderColor: theme.fontcolor }]} placeholder="Identifiant de la serrure" value={identifier} onChangeText={setIdentifier} />
+                        </View>
+                        <View style={styles.innerInputContainer}>
+                            <Text style={[styles.subtitle, { color: theme.fontcolor }]}>Mot de passe de la serrure</Text>
+                            <TextInput style={[styles.inputContainer, { borderColor: theme.fontcolor }]} secureTextEntry={true} placeholder="mot de passe de la serrure" defaultValue="password" value={password} onChangeText={setPassword} />
+                        </View>
+                        <View style={styles.innerInputContainer}>
+                            <Text style={[styles.subtitle, { color: theme.fontcolor }]}>Label personnalisé</Text>
+                            <TextInput style={[styles.inputContainer, { borderColor: theme.fontcolor }]} placeholder="Label de votre serrure" value={label} onChangeText={setLabel} />
+                        </View>
 
-                    <Pressable style={[styles.ButtonContainer, { borderColor: theme.fontcolor }]} onPress={() => { onClose() }}>
-                        <Text style={{ color: theme.fontcolor }}>Ajouter</Text>
-                    </Pressable>
+                        <Pressable style={[styles.ButtonContainer, { borderColor: theme.fontcolor }]} onPress={() => { onClose() }}>
+                            <Text style={{ color: theme.fontcolor }}>Ajouter</Text>
+                        </Pressable>
+                    </View>
                 </View>
-            </View>
+            </TouchableWithoutFeedback >
 
         </Modal >
     )
