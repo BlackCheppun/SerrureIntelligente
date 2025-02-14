529d0afcca126dc8007c4c4a91bf36c506d9d60a
BlackCheppun <aghouilesilyes1@gmail.com>
2025-02-14 10:17:28 +0100

    implemented warning modal, and api add lock api

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -5,6 +5,7 @@ import { darkTheme, lightTheme } from "../types/themes";
 import { lockService } from "../services/lock.service";
 import { Key, useEffect, useState } from "react";
 import AddLockModal from "../components/AddLockModal";
+import WarningModal from "../components/Warningmodal";
 
 interface props {
     navigation: NavigationProp<ParamListBase>;
@@ -31,11 +32,28 @@ export default function Home({ navigation }: props) {
     const [label, setLabel] = useState<string>('');
     const [modalVisible, setModalVisible] = useState<boolean>(false);
 
+    const [warningVisible, setWarningVisible] = useState<boolean>(false);
+    const [warningLabel, setWarningLabel] = useState<string>('');
+
+    const addlock = async () => {
+        if (identifier == '' || password == '' || label == '') {
+            setWarningLabel('Veuillez remplir tous les champs');
+            setWarningVisible(true);
+        }
+        else {
+            const res = await lockService.addLock(identifier, password, label);
+            if (res) {
+                setModalVisible(false);
+            }
+        }
+    }
+
 
     return (
 
         <View style={{ flex: 1, backgroundColor: theme.background }} >
-            <AddLockModal visible={modalVisible} onClose={() => { setModalVisible(false) }} identifier={identifier} label={label} password={password} setIdentifier={(text) => setIdentifier(text)} setLabel={(text) => setLabel(text)} setPassword={(text) => setPassword(text)} />
+            <AddLockModal setVisible={(val) => setModalVisible(val)} visible={modalVisible} onClose={() => { addlock() }} identifier={identifier} label={label} password={password} setIdentifier={(text) => setIdentifier(text)} setLabel={(text) => setLabel(text)} setPassword={(text) => setPassword(text)} />
+            <WarningModal label={warningLabel} visible={warningVisible} onClose={() => setWarningVisible(false)} />
             <View style={styles.TopContainer}>
                 <Text style={[styles.Title, { color: theme.fontcolor }]}>Bonjour</Text>
                 <Pressable onPress={() => setModalVisible(true)}>
