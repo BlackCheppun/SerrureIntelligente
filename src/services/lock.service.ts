68f05f684e17027756fe09e6fb7bf080a33e72ff
BlackCheppun <aghouilesilyes1@gmail.com>
2025-03-28 22:01:27 +0100

    fix : edit body params of add lock post http req

--- src/services/lock.service.ts
+++ src/services/lock.service.ts
@@ -55,13 +55,16 @@ export class lockService {
     static async addLock(identifier: string, password: string, label: string) {
         try {
             const user_id = await mmkv.getStringAsync("user_id");
-            const response = await axiosClient.post("/addlock", {
-                userid: user_id,
-                lockid: identifier,
-                password: password,
-                label: label
+            const response = await axiosClient.post("/addlock", null, {
+                params: {
+                    userid: user_id,
+                    lockid: identifier,
+                    password: password,
+                    label: label
+                }
             });
             if (response.status == 200) {
+                await this.getLocks();
                 return true;
             }
             else {
